533ee7b7e20e39d53c39cb94dcaa96149021cc0a
Hugo19S <adyseta1998@outlook.com>
2023-11-28 17:24:43 +0000

    Final

--- src/pages/Home/components/AboutMeSection/index.tsx
+++ src/pages/Home/components/AboutMeSection/index.tsx
@@ -14,15 +14,16 @@ const AboutMeSection = ({ isMobile }: AboutMeContract) => {
                 <S.AboutMeContainer>
                     <div>
                         <p>
-                        Olá, sou Hugo Furtado, um entusiasta de tecnologia que decidiu
-                        seguir o caminho da engenharia informática para aprimorar minhas 
-                        habilidades em programação. Ao longo da minha jornada, estive 
-                        envolvido em projetos diversos, particularmente focados na gestão 
-                        educacional, desenvolvimento de API's, administração de redes e administração de bancos de dados.
-                        Essa experiência permitiu-me explorar as complexidades do desenvolvimento de software, visando não 
-                        apenas a eficácia operacional, mas também o impacto positivo nos sistemas educacionais. Estou comprometido
-                        em continuar aprendendo e aplicando meus conhecimentos para contribuir com soluções inovadoras e eficazes
-                        no campo da tecnologia.
+                        Olá, sou Hugo Furtado, um entusiasta de tecnologia que decidiu seguir o caminho da 
+                        engenharia informática para aprimorar minhas habilidades em programação. Ao longo da minha jornada, estive 
+                        envolvido em projetos diversos, particularmente focados em criar sites, administração de redes e 
+                        administração de bancos de dados. Essa experiência permitiu-me explorar as complexidades do desenvolvimento 
+                        de software, visando a eficácia operacional. Estou comprometido em continuar aprendendo e aplicando 
+                        meus conhecimentos 
+                        para contribuir com soluções inovadoras e eficazes no campo da tecnologia. O que mais gosto de fazer nos 
+                        meus tempos livres é jogar bola ou assistir a jogos de futebol. Adoro a emoção e a energia que o esporte 
+                        proporciona. Além disso, a prática regular me ajuda a relaxar, me mantém ativo e me permite socializar com 
+                        amigos que compartilham do mesmo interesse.
                         </p>
                         <br />
                         <p>
